23ffc9ce41d400eb36cd6aea18fbdc614571b721
Imam Saputra <imam.saputra@brilife.co.id>
2023-06-21 21:42:06 +0700

    Update

---
+++ Test Suites/NON LOGIN/Surfing/TSC_P_Surfing.ts
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteCollectionEntity>
+   <description></description>
+   <name>TSC_P_Surfing</name>
+   <tag></tag>
+   <delayBetweenInstances>0</delayBetweenInstances>
+   <executionMode>SEQUENTIAL</executionMode>
+   <maxConcurrentInstances>8</maxConcurrentInstances>
+   <testSuiteRunConfigurations>
+      <TestSuiteRunConfiguration>
+         <configuration>
+            <groupName>Mobile</groupName>
+            <profileName>default</profileName>
+            <requireConfigurationData>true</requireConfigurationData>
+            <runConfigurationData>
+               <entry>
+                  <key>deviceName</key>
+                  <value>samsung SM-G991B (Android 13)</value>
+               </entry>
+               <entry>
+                  <key>deviceId</key>
+                  <value>RRCR700EWZJ</value>
+               </entry>
+            </runConfigurationData>
+            <runConfigurationId>Android</runConfigurationId>
+         </configuration>
+         <runEnabled>true</runEnabled>
+         <testSuiteEntity>Test Suites/NON LOGIN/Surfing/TS_P_Surfing</testSuiteEntity>
+      </TestSuiteRunConfiguration>
+   </testSuiteRunConfigurations>
+</TestSuiteCollectionEntity>
--- Test Suites/NON LOGIN/Surfing/TS_P_Surfing.ts
+++ Test Suites/NON LOGIN/Surfing/TS_P_Surfing.ts
@@ -11,4 +11,11 @@
    <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
    <rerunImmediately>false</rerunImmediately>
    <testSuiteGuid>ed5a2bcd-c082-465e-a910-0c527e37cc3a</testSuiteGuid>
+   <testCaseLink>
+      <guid>921b80f1-9a15-43fc-a08c-ab42874e8023</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/NON LOGIN/TC_P_Surfing</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
 </TestSuiteEntity>
